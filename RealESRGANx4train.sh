f638a9c94894cef3b4f88fa4ba560e22548ea65d
m.aleshin <m.aleshin@an01.zhores>
2024-09-01 00:29:47 +0300

    upd scripts

--- RealESRGANx4train.sh
+++ RealESRGANx4train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=ESRGAN       # create a short name for your job
+#SBATCH --job-name=ESRGAN4       # create a short name for your job
 #SBATCH --partition=ais-gpu 
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks-per-node=1      # total number of tasks per node
-#SBATCH --gpus=4                 # 4
+#SBATCH --gpus=8                 # 4
 #SBATCH --cpus-per-task=64       # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=12G
 #SBATCH --time=10-00:00:00       # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
@@ -19,6 +19,7 @@ conda activate esrgan
 cd /beegfs/home/m.aleshin/projects/superresolution/Real-ESRGAN
 
 export OMP_NUM_THREADS=8
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 
 
 # nvidia-smi
 
@@ -30,10 +31,9 @@ export OMP_NUM_THREADS=8
 #                         --launcher pytorch --auto_resume
 
 # 2
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python -m torch.distributed.launch \
-    --nproc_per_node=4 \
-    --master_port=13110 \
+    --nproc_per_node=8 \
+    --master_port=13114 \
     realesrgan/train.py -opt options/train_realesrgan_x4plus_mod.yml \
                         --launcher pytorch --auto_resume
 
--- RealESRNETx4train.sh
+++ RealESRNETx4train.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks-per-node=1      # total number of tasks per node
 #SBATCH --gpus=4
-#SBATCH --cpus-per-task=64         
-#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=32         
+#SBATCH --mem-per-cpu=12G
 #SBATCH --time=7-00:00:00       # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
@@ -23,12 +23,12 @@ cd /beegfs/home/m.aleshin/projects/superresolution/Real-ESRGAN
 
 # nvidia-smi
 
-export OMP_NUM_THREADS=8
+export OMP_NUM_THREADS=2
 # 1
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python -m torch.distributed.launch \
     --nproc_per_node=4 \
-    --master_port=13110 \
+    --master_port=13114 \
     realesrgan/train.py -opt options/train_realesrnet_x4plus_mod.yml \
                         --launcher pytorch --auto_resume #--debug
 
