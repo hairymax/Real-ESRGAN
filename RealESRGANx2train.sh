87e829cb67c48e3da02dbf0b7aa8faf584e1c3bd
m.aleshin <m.aleshin@an01.zhores>
2024-09-15 22:22:40 +0300

    upd

--- RealESRGANx2train.sh
+++ RealESRGANx2train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=ESRGAN4       # create a short name for your job
-#SBATCH --partition=ais-gpu
+#SBATCH --job-name=ESRGAN2       # create a short name for your job
+#SBATCH --partition=ais-gpu 
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks-per-node=1      # total number of tasks per node
 #SBATCH --gpus=4                 # 4
 #SBATCH --cpus-per-task=32       # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=12G
-#SBATCH --time=10-00:00:00       # total run time limit (HH:MM:SS)
+#SBATCH --time=10-00:00:00        # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-type=fail         # send email when job fails
@@ -23,7 +23,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3 #,4,5,6,7
 
 python -m torch.distributed.launch \
     --nproc_per_node=4 \
-    --master_port=13144 \
-    realesrgan/train.py -opt options/finetune_realesrgan_x4plus_mod.yml \
+    --master_port=13142 \
+    realesrgan/train.py -opt options/finetune_realesrgan_x2plus_mod.yml \
                         --launcher pytorch --auto_resume
-    # realesrgan/train.py -opt options/train_realesrgan_x4plus_mod.yml \
+    # realesrgan/train.py -opt options/train_realesrgan_x2plus_mod.yml \
